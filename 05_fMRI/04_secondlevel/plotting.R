a8c54a37364c189b9ead7f4c5a47b9a3ad9a00c1
Milena Musial <m.musial@outlook.de>
2025-09-08 10:59:29 +0200

    plotting and exploratory analyses

--- 05_fMRI/04_secondlevel/plotting.R
+++ 05_fMRI/04_secondlevel/plotting.R
@@ -10,8 +10,7 @@ sapply(libs, require, character.only=TRUE)
 # define paths
 data_path <- 'WP2_ILT_DATA'
 
-nacc_main <- read.csv(file.path(data_path, "fMRI/PH_withC/MAIN_RPE_NAcc_per_reinforcer_across_groups.txt"))
-nacc_atc <- read.csv(file.path(data_path, "fMRI/PH_withC/ATC_RPE_NAcc_per_reinforcer_across_groups.txt"))
+nacc_main <- read.csv(file.path(data_path, "fMRI/PH_withC/MAIN_RPE_NAcc_per_reinforcer_across_groups_t1.txt"))
 load(file.path(data_path, "Behav/behav_final_redcap_n56.RData"))
 
 ###### Main Plot: NAcc activity per group and reinforcer type ######
@@ -37,7 +36,7 @@ fig_main_left_nacc <- ggplot(nacc_main_left, aes(x=reinforcer, y=value, fill=rei
   geom_boxplot(width = 0.1, size=0.8) +
   facet_wrap(~aud_group, scales="fixed") +
   xlab("group") +
-  ylab("Mean RPE-related activity \n Left NAcc") +
+  ylab("Mean RPE-related activity \n in left VS") +
   ylim(-0.1, 0.08) +
   theme_light(base_size = 18) +
   theme(legend.position = "right") +
@@ -54,7 +53,7 @@ fig_main_right_nacc <- ggplot(nacc_main_right, aes(x=reinforcer, y=value, fill=r
   geom_boxplot(width = 0.1, size=0.8) +
   facet_wrap(~aud_group, scales="fixed") +
   xlab("group") +
-  ylab("Mean RPE-related activity \n Right NAcc") +
+  ylab("Mean RPE-related activity \n in right VS") +
   ylim(-0.1, 0.08) +
   theme_light(base_size = 18) +
   theme(legend.position = "right") +
@@ -67,7 +66,7 @@ fig_main_right_nacc <- ggplot(nacc_main_right, aes(x=reinforcer, y=value, fill=r
                     palette = "Accent")
 
 main_peak_plot <- ggarrange(fig_main_left_nacc, fig_main_right_nacc,
-          labels = c("B", "C"),
+          labels = c("C", "D"),
           ncol = 2, nrow = 1,
           common.legend = TRUE, legend="bottom")
 
@@ -75,63 +74,3 @@ png(file.path("Manuscript/Figures", "NAcc_main.png"), width = 18, height = 10, u
 main_peak_plot
 dev.off()
 
-###### Exploratory plot: Correlation NAcc alc>jui in AUD - psychometric measures
-
-# structure data
-nacc_atc <- nacc_atc %>%
-  mutate(ID = c(1:56),
-         aud_group = as.factor(c(rep("HC",28), rep("AUD",28)))) %>%
-  pivot_longer(!c("ID", "aud_group"), names_to = "combination", values_to = "value") %>%
-  mutate(combination = as.factor(combination),
-         reinforcer = case_when(combination %in% c("left_nacc_alc", "right_nacc_alc") ~ "alcohol",
-                                combination %in% c("left_nacc_jui", "right_nacc_jui") ~ "juice"),
-         region = case_when(combination %in% c("left_nacc_alc", "left_nacc_jui") ~ "left_Nacc",
-                            combination %in% c("right_nacc_jui", "right_nacc_alc") ~ "right_Nacc"))
-
-nacc_atc_left <- nacc_atc %>%
-  filter(region == "left_Nacc")
-nacc_atc_right <- nacc_atc %>%
-  filter(region == "right_Nacc")
-
-fig_atc_left_nacc <- ggplot(nacc_atc_left, aes(x=reinforcer, y=value, fill=reinforcer)) +
-  geom_violin() +
-  geom_boxplot(width = 0.1, size=0.8) +
-  facet_wrap(~aud_group, scales="fixed") +
-  xlab("group") +
-  ylab("Mean RPE-related activity \n Left NAcc") +
-  ylim(-0.1, 0.08) +
-  theme_light(base_size = 18) +
-  theme(legend.position = "right") +
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position = "bottom") +
-  scale_fill_brewer(name="Reinforcer type", 
-                    labels=c("alcohol", "juice"),
-                    palette = "Accent")
-
-fig_atc_right_nacc <- ggplot(nacc_atc_right, aes(x=reinforcer, y=value, fill=reinforcer)) +
-  geom_violin() +
-  geom_boxplot(width = 0.1, size=0.8) +
-  facet_wrap(~aud_group, scales="fixed") +
-  xlab("group") +
-  ylab("Mean RPE-related activity \n Right NAcc") +
-  ylim(-0.1, 0.08) +
-  theme_light(base_size = 18) +
-  theme(legend.position = "right") +
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position = "bottom") +
-  scale_fill_brewer(name="Reinforcer type", 
-                    labels=c("alcohol", "juice"),
-                    palette = "Accent")
-
-atc_peak_plot <- ggarrange(fig_atc_left_nacc, fig_atc_right_nacc,
-                            labels = c("B", "C"),
-                            ncol = 2, nrow = 1,
-                            common.legend = TRUE, legend="bottom")
-
-png("NAcc_atc.png", width = 18, height = 10, units='cm', res = 600)
-atc_peak_plot
-dev.off()
